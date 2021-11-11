5dba42a17e2c08e7d2eac121d1cfbeaf9a013f8e
myusername <myusername@myemaildomain.com>
2021-11-11 09:09:35 +0300

    docker-compose fl md

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
+client = MongoClient(host="localhost", port=27017)
+client = MongoClient("mongodb://localhost:27017")
+db = client["rptutorials"]
 @app.route("/")
 def index():
     return "<p>Hello, World!</p>"
