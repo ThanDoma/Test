b83f0252972cb3bbceae78b4c8788b41717ed700
Anton Holmes <antonholmes48@gmail.com>
2021-11-14 18:26:19 +0300

    connect db

--- app.py
+++ app.py
@@ -4,31 +4,25 @@ import flask
 
 
 app = Flask(__name__)
+#### ConnectDB 
 # app.config["MONGO_URI"] = "mongodb://devroot:devroot@localhost:27017/?authSource=admin&readPreference=primary&ssl=false"
-app.config["MONGO_URI"] = "mongodb://devroot:devroot@localhost:27017/admin"
+app.config["MONGO_URI"] = "mongodb://devroot:devroot@localhost:27017/mydb?authSource=admin"
 mongodb_client = PyMongo(app)
 db = mongodb_client.db
 
 
-# @app.route("/")
-# def index():
-#     return render_template("index.html")
+@app.route("/")
+def index():
+    return render_template("index.html")
 
-# @app.route("/")
-# def home_page():
-#     online_users = mongo.db.users.find({"online": True})
-#     return render_template(online_users=online_users)
 
 @app.route("/add_one")
 def add_one():
     db.to.insert_one({'title': "todo title", 'body': "todo body"})
     return flask.jsonify(message="success")
 
-# @app.route("/add_one")
-# def add_one():
-#     one = mongo.db.collection.find()
-#     # one = mongo.mydb.insert_one({'title': "todo title", 'body': "todo body"})
-#     return one
-
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
